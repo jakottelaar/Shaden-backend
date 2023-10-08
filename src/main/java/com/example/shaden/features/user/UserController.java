285bc2950e1eb0eb7d0418a1d6aa7e5f54187c0d
jakottelaar <jarno.kottelaar@gmail.com>
2023-10-09 00:06:18 +0200

    Updated workflow.yml
    Created application-prod-test.yml file

--- src/main/java/com/example/shaden/features/user/UserController.java
+++ src/main/java/com/example/shaden/features/user/UserController.java
@@ -2,10 +2,8 @@ package com.example.shaden.features.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
