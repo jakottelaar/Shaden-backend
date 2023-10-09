47d72e438b8a31d44415b9876c2ae69fa502f52e
jakottelaar <jarno.kottelaar@gmail.com>
2023-10-09 22:42:59 +0200

    Created duplicate account and refresh endpoint test

--- src/test/java/com/example/shaden/authentication/AuthIntegrationTest.java
+++ src/test/java/com/example/shaden/authentication/AuthIntegrationTest.java
@@ -1,12 +1,9 @@
 package com.example.shaden.authentication;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,7 +24,6 @@ public class AuthIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
     private static String accessToken;
-   	private static final Logger LOG = LoggerFactory.getLogger(AuthIntegrationTest.class);
 
     @Test
     @Order(1)
@@ -39,9 +35,20 @@ public class AuthIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
     }
-   
+
     @Test
     @Order(2)
+    public void accountWithEmailAlreadyExists() throws Exception {
+        String jsonRequest = "{\"username\":\"testUsername\",\"email\":\"test@mail.com\",\"password\":\"Testpass1234\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+                .content(jsonRequest)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict());
+    }
+   
+    @Test
+    @Order(3)
     public void testLogin() throws Exception {
         String jsonRequest = "{\"email\":\"test@mail.com\",\"password\":\"Testpass1234\"}";
    
@@ -56,9 +63,26 @@ public class AuthIntegrationTest {
         accessToken = jsonResponse.getString("access_token");
 
     }
-   
+
     @Test
-    @Order(3)
+    @Order(4)
+    public void testRefreshToken() throws Exception {
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/refresh-token")
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        
+        String responseBody = result.getResponse().getContentAsString();
+        assert(responseBody.contains("access_token"));
+        assert(responseBody.contains("refresh_token"));
+
+    }
+
+
+    @Test
+    @Order(5)
     public void deleteRegisteredUser() throws Exception {
    
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/users/delete-account")
