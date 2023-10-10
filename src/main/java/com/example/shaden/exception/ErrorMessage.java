5629c8497bb1b0cad3b285aa213ad3bb74e12518
jakottelaar <jarno.kottelaar@gmail.com>
2023-10-10 16:32:15 +0200

    Update error message set json date format.

--- src/main/java/com/example/shaden/exception/ErrorMessage.java
+++ src/main/java/com/example/shaden/exception/ErrorMessage.java
@@ -3,12 +3,16 @@ package com.example.shaden.exception;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class ErrorMessage {
     
     private int statusCode;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     private Date timestamp;
     private String message;
     private List<String> errors;
