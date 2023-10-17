b2e576481274e223d3d82677fcb30fd06e9b29c8
jakottelaar <jarno.kottelaar@gmail.com>
2023-10-17 11:44:46 +0200

    Updated ResponseData
    ResponseData  now contains jsonproperty "status" instead of statusCode.

--- src/main/java/com/example/shaden/features/ResponseData.java
+++ src/main/java/com/example/shaden/features/ResponseData.java
@@ -1,10 +1,13 @@
 package com.example.shaden.features;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class ResponseData {
     
+    @JsonProperty("status")
     private int statusCode;
     private String message;
     private Object results;
