4b3bc190e7e851f7ce0aded6b08957cc01fe869a
jakottelaar <jarno.kottelaar@gmail.com>
2023-10-19 15:23:40 +0200

    Quick hotfixes

--- src/main/java/com/example/shaden/features/ResponseData.java
+++ src/main/java/com/example/shaden/features/ResponseData.java
@@ -1,10 +1,12 @@
 package com.example.shaden.features;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import lombok.Data;
 
 @Data
+@JsonPropertyOrder({"status", "message", "results"})
 public class ResponseData {
     
     @JsonProperty("status")
