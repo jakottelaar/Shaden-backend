b8729951080d4fb9a8a0650161312e0bcfce2454
jakottelaar <jarno.kottelaar@gmail.com>
2023-10-20 17:42:10 +0200

    Updated get all friends endpoint

--- src/main/java/com/example/shaden/features/friend/FriendRepository.java
+++ src/main/java/com/example/shaden/features/friend/FriendRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.shaden.features.user.User;
 
@@ -13,8 +14,6 @@ import jakarta.transaction.Transactional;
 public interface FriendRepository extends JpaRepository<Friendship, Long>{
 
     Friendship findByFriend1AndFriend2(User user, User friend);
-
-    List<Friendship> findAllByFriend1OrFriend2(User user, User user2);
     
     @Transactional
     @Modifying(clearAutomatically = true)
@@ -22,4 +21,7 @@ public interface FriendRepository extends JpaRepository<Friendship, Long>{
     void updateFriendShipStatus(Long id, FriendshipStatus status);
 
     List<Friendship> findAllByFriend1AndStatus(User user, FriendshipStatus pending);
+
+    @Query("SELECT f FROM Friendship f WHERE (f.friend1 = :user OR f.friend2 = :user) AND f.status = :status")
+    List<Friendship> findAllFriendsByStatus(@Param("user") User user, @Param("status") FriendshipStatus status);
 }
--- src/main/java/com/example/shaden/features/friend/FriendService.java
+++ src/main/java/com/example/shaden/features/friend/FriendService.java
@@ -56,17 +56,17 @@ public class FriendService {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         UserPrincipal user = (UserPrincipal) auth.getPrincipal();
-
-        List<Friendship> friendships = friendRepository.findAllByFriend1OrFriend2(user.getUser(), user.getUser());
-
+    
+        List<Friendship> friendships = friendRepository.findAllFriendsByStatus(user.getUser(), FriendshipStatus.ACCEPTED);
+    
         List<FriendResponse> friendResponses = friendships.stream()
             .map(friendship -> {
                 User friend = friendship.getFriend1().equals(user.getUser()) ? friendship.getFriend2() : friendship.getFriend1();
-
+    
                 return new FriendResponse(friend.getId(), friend.getUsername());
             })
             .collect(Collectors.toList());
-
+    
         return friendResponses;
     }
 
