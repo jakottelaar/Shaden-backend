7ae218b22704fcbbc60209c63f26f2ac369dde1e
jakottelaar <jarno.kottelaar@gmail.com>
2023-10-21 23:10:24 +0200

    Created search users tests

--- src/test/java/com/example/shaden/users/UserIntegrationTests.java
+++ src/test/java/com/example/shaden/users/UserIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.example.shaden.users;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -19,6 +20,7 @@ import com.example.shaden.config.JsonParserUtil;
 import com.example.shaden.features.authentication.AuthenticationService;
 import com.example.shaden.features.authentication.request.AuthenticationRequest;
 import com.example.shaden.features.authentication.request.RegisterRequest;
+import com.example.shaden.features.user.UserRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -33,18 +35,31 @@ public class UserIntegrationTests {
     @Autowired
     private AuthenticationService authenticationService;
 
-    private static String accessToken;
+    @Autowired
+    private UserRepository userRepository;
+
+    private static String accessTokenUser1;
+    private static String accessTokenUser2;
 
     @BeforeAll
     public void setup() throws Exception {
         
-        RegisterRequest testUser = new RegisterRequest("testUser1","test1@mail.com", "testMan1");
+        RegisterRequest testUser = new RegisterRequest("testUser1","testuser1@mail.com", "testMan1");
+        RegisterRequest testUser2 = new RegisterRequest("testUser2","testuser2@mail.com", "testMan2");
         authenticationService.register(testUser);
+        authenticationService.register(testUser2);
 
-        AuthenticationRequest authenticationRequest = new AuthenticationRequest("test1@mail.com", "testMan1");
+        AuthenticationRequest authenticationRequestUser1 = new AuthenticationRequest("testuser1@mail.com", "testMan1");
+        AuthenticationRequest authenticationRequestUser2 = new AuthenticationRequest("testuser2@mail.com", "testMan2");
 
-        accessToken = authenticationService.authenticate(authenticationRequest).getAccessToken();
+        accessTokenUser1 = authenticationService.authenticate(authenticationRequestUser1).getAccessToken();
+        accessTokenUser2 = authenticationService.authenticate(authenticationRequestUser2).getAccessToken();
 
+    }
+
+    @AfterAll
+    public void teardown() throws Exception {
+        userRepository.deleteById(userRepository.findByEmail("testuser2@mail.com").get().getId());
     }
 
     @Test
@@ -52,7 +67,7 @@ public class UserIntegrationTests {
     public void testGetUserProfile() throws Exception {
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/users/profile")
-                .header("Authorization", "Bearer " + accessToken))
+                .header("Authorization", "Bearer " + accessTokenUser1))
                 .andExpect(status().isOk())
                 .andReturn();
 
@@ -61,17 +76,51 @@ public class UserIntegrationTests {
         assert(jsonResponse.get("status").asInt() == 200);
         assert(jsonResponse.get("message").asText().equals("Successfully retrieved user profile"));
         assert(jsonResponse.get("results").get("username").asText().equals("testUser1"));
-        assert(jsonResponse.get("results").get("email").asText().equals("test1@mail.com"));
+        assert(jsonResponse.get("results").get("email").asText().equals("testuser1@mail.com"));
     }
 
     @Test
     @Order(2)
+    public void testUser2SearchUser1() throws Exception {
+            MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/users/search?username=testUser1")
+                    .header("Authorization", "Bearer " + accessTokenUser2))
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+            JsonNode jsonResponse = JsonParserUtil.parseJsonResponse(result);
+
+            assert(jsonResponse.get("status").asInt() == 200);
+            assert(jsonResponse.get("message").asText().equals("Successfully retrieved users"));
+            assert(jsonResponse.get("results").get(0).get("username").asText().equals("testUser1"));
+            assert(jsonResponse.get("results").get(0).get("email").asText().equals("testuser1@mail.com"));
+    }
+
+    @Test
+    @Order(2)
+    public void testUser1SearchAllUsers() throws Exception {
+            MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/users/search?username=testUser")
+                    .header("Authorization", "Bearer " + accessTokenUser2))
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+            JsonNode jsonResponse = JsonParserUtil.parseJsonResponse(result);
+
+            assert(jsonResponse.get("status").asInt() == 200);
+            assert(jsonResponse.get("message").asText().equals("Successfully retrieved users"));
+            assert(jsonResponse.get("results").get(0).get("username").asText().equals("testUser1"));
+            assert(jsonResponse.get("results").get(0).get("email").asText().equals("testuser1@mail.com"));
+            assert(jsonResponse.get("results").get(1).get("username").asText().equals("testUser2"));
+            assert(jsonResponse.get("results").get(1).get("email").asText().equals("testuser2@mail.com"));
+    }
+
+    @Test
+    @Order(3)
     public void testUpdateUserProfile() throws Exception {
             
         String jsonRequest = "{\"username\":\"testUser1Changed\",\"email\":\"test1changed@mail.com\"}";
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.patch("/api/users/profile")
-                .header("Authorization", "Bearer " + accessToken)
+                .header("Authorization", "Bearer " + accessTokenUser1)
                 .contentType("application/json")
                 .content(jsonRequest))
                 .andExpect(status().isOk())
@@ -86,14 +135,12 @@ public class UserIntegrationTests {
 
     }
 
-        
-
     @Test
-    @Order(2)
+    @Order(4)
     public void testDeleteUserAccount() throws Exception {
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/api/users/profile")
-                .header("Authorization", "Bearer " + accessToken))
+                .header("Authorization", "Bearer " + accessTokenUser1))
                 .andExpect(status().isNoContent())
                 .andReturn();
 
