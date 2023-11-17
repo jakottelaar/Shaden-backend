3dc0eabb91128605aee7eaa6d2635337b9ff4297
jakottelaar <jarno.kottelaar@gmail.com>
2023-11-17 12:59:12 +0100

    Removed TestUserConfig and updated dm tests

--- src/test/java/com/example/shaden/channel/dm/DirectMessageChannelIntegrationTests.java
+++ src/test/java/com/example/shaden/channel/dm/DirectMessageChannelIntegrationTests.java
@@ -3,6 +3,7 @@ package com.example.shaden.channel.dm;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,6 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -21,6 +21,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.example.shaden.config.JsonParserUtil;
+import com.example.shaden.features.authentication.AuthenticationService;
+import com.example.shaden.features.authentication.request.AuthenticationRequest;
+import com.example.shaden.features.authentication.request.RegisterRequest;
 import com.example.shaden.features.channel.dm.DMChannelRepository;
 import com.example.shaden.features.channel.dm.request.CreateDmChannelRequest;
 import com.example.shaden.features.user.User;
@@ -38,28 +41,40 @@ public class DirectMessageChannelIntegrationTests {
     private MockMvc mockMvc;
 
     @Autowired
-    @Qualifier("getTestUserToken1")
-    private String testUserToken1;
-
-    @Autowired
-    @Qualifier("getTestUserToken1")
-    private String testUserToken2;
-
-    @Autowired
-    @Qualifier("getTestUser1")
-    private User testFriend1;
-
-    @Autowired
-    @Qualifier("getTestUser2")
-    private User testFriend2;
-
-    @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private DMChannelRepository dmChannelRepository;
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     private Logger LOGGER = LoggerFactory.getLogger(DirectMessageChannelIntegrationTests.class);
+
+    private static String testUserToken1;
+    private static String testUserToken2;
+    private static User testFriend1;
+    private static User testFriend2;
+
+    @BeforeAll
+    public void setup() throws Exception {
+        // Create test users
+        RegisterRequest testUser1 = new RegisterRequest("testUser1", "testuser1@mail.com", "testMan1");
+        RegisterRequest testUser2 = new RegisterRequest("testUser2", "testuser2@mail.com", "testMan2");
+        authenticationService.register(testUser1);
+        authenticationService.register(testUser2);
+
+        // Authenticate test users
+        AuthenticationRequest authenticationRequestUser1 = new AuthenticationRequest("testuser1@mail.com", "testMan1");
+        AuthenticationRequest authenticationRequestUser2 = new AuthenticationRequest("testuser2@mail.com", "testMan2");
+
+        testUserToken1 = authenticationService.authenticate(authenticationRequestUser1).getAccessToken();
+        testUserToken2 = authenticationService.authenticate(authenticationRequestUser2).getAccessToken();
+
+        // Retrieve test users from the repository
+        testFriend1 = userRepository.findByEmail("testuser1@mail.com").orElse(null);
+        testFriend2 = userRepository.findByEmail("testuser2@mail.com").orElse(null);
+    }
 
     @AfterAll
     public void tearDown() {
