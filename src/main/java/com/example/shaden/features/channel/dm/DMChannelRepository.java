071c47761a77319009ec2d1aacae3a2fb1f894d2
jakottelaar <jarno.kottelaar@gmail.com>
2023-11-17 16:41:45 +0100

    Created get all channels for a user endpoint

--- src/main/java/com/example/shaden/features/channel/dm/DMChannelRepository.java
+++ src/main/java/com/example/shaden/features/channel/dm/DMChannelRepository.java
@@ -1,5 +1,7 @@
 package com.example.shaden.features.channel.dm;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -7,5 +9,8 @@ public interface DMChannelRepository extends JpaRepository<DMChannel, Long>{
 
     @Query("SELECT dm FROM DMChannel dm WHERE (dm.user1.id = ?1 AND dm.user2.id = ?2) OR (dm.user1.id = ?2 AND dm.user2.id = ?1)")
     DMChannel findDMChannelByUser1IdAndUser2Id(Long user1Id, Long user2Id);
+
+    @Query("SELECT dm FROM DMChannel dm WHERE dm.user1.id = ?1 OR dm.user2.id = ?1")
+    List<DMChannel> findAllByUserId(long userId);
 
 }
