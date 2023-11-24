cf090cf4a872594cdcda14ddaaa2cf4aae4b78d7
jakottelaar <jarno.kottelaar@gmail.com>
2023-11-24 19:38:48 +0100

    Updated updates to use andExpect and jsonPath
    Cleaned up a lot of test code

--- src/test/java/com/example/shaden/authentication/AuthIntegrationTest.java
+++ src/test/java/com/example/shaden/authentication/AuthIntegrationTest.java
@@ -15,14 +15,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.example.shaden.config.JsonParserUtil;
 import com.example.shaden.features.authentication.request.AuthenticationRequest;
 import com.example.shaden.features.authentication.request.RegisterRequest;
 import com.example.shaden.features.user.UserRepository;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.Matchers;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
@@ -40,6 +43,8 @@ public class AuthIntegrationTest {
     private static String accessToken;
     private static String refreshToken;
 
+    private Gson gson = new Gson();
+
     @AfterAll
     public void cleanUp() throws Exception {
         userRepository.deleteAll();
@@ -53,138 +58,113 @@ public class AuthIntegrationTest {
         registerRequest.setUsername("authTestUsername");
         registerRequest.setEmail("authtest@mail.com");
         registerRequest.setPassword("Testpass1234");
-   
-        MvcResult result =  mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
-                .content(JsonParserUtil.asJsonString(registerRequest))
+    
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+                .content(gson.toJson(registerRequest))
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated()).andReturn();
-
-        String jsonResponse = result.getResponse().getContentAsString();
-        JsonObject parsedResponse = JsonParser.parseString(jsonResponse).getAsJsonObject();
-
-        assert(parsedResponse.get("status").getAsInt() == 201);
-        assert(parsedResponse.get("message").getAsString().contains("User registered successfully"));
-        assert(parsedResponse.get("results").getAsJsonObject().get("access_token").getAsString() != null);
-        assert(parsedResponse.get("results").getAsJsonObject().get("refresh_token").getAsString() != null);
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.status").value(201))
+                .andExpect(jsonPath("$.message").value("User registered successfully"))
+                .andExpect(jsonPath("$.results.access_token").isString())
+                .andExpect(jsonPath("$.results.refresh_token").isString())
+                .andReturn();
     }
-
-    //Test invalid input for account registration
-    @Test
-    @Order(2)
-    public void invalidInputAccountRegistration() throws Exception {
-        RegisterRequest registerRequest = new RegisterRequest();
-        registerRequest.setUsername("");
-        registerRequest.setEmail("testmail.com");
-        registerRequest.setPassword("Testpass");
     
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
-                .content(JsonParserUtil.asJsonString(registerRequest))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andReturn();
+// Test invalid input for account registration
+@Test
+@Order(2)
+public void invalidInputAccountRegistration() throws Exception {
+    RegisterRequest registerRequest = new RegisterRequest();
+    registerRequest.setUsername("");
+    registerRequest.setEmail("testmail.com");
+    registerRequest.setPassword("Testpass");
 
-        String jsonResponse = result.getResponse().getContentAsString();
-        JsonObject parsedResponse = JsonParser.parseString(jsonResponse).getAsJsonObject();
-        
-        assert(parsedResponse.get("status").getAsInt() == 400);
-        assert(parsedResponse.get("message").getAsString().contains("Validation failed"));
-        assert(parsedResponse.get("errors").toString().contains("Username is required"));
-        assert(parsedResponse.get("errors").toString().contains("Invalid email format"));
-        assert(parsedResponse.get("errors").toString().contains("Password must contain at least one letter, one digit, and be 8 or more characters long"));
-    }
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+        .content(gson.toJson(registerRequest))
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.status").value(400))
+        .andExpect(jsonPath("$.message").value("Validation failed"))
+        .andExpect(jsonPath("$.errors").isArray())
+        .andExpect(jsonPath("$.errors", Matchers.containsInAnyOrder(
+                "Invalid email format",
+                "Username is required",
+                "Password must contain at least one letter, one digit, and be 8 or more characters long"
+        )));
 
-    //Test account registration with email that already exists
-    @Test
-    @Order(3)
-    public void accountWithEmailAlreadyExists() throws Exception {
-        RegisterRequest registerRequest = new RegisterRequest();
-        registerRequest.setUsername("authTestUsername");
-        registerRequest.setEmail("authtest@mail.com");
-        registerRequest.setPassword("Testpass1234");
+}
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
-                .content(JsonParserUtil.asJsonString(registerRequest))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isConflict()).andReturn();
+// Test account registration with email that already exists
+@Test
+@Order(3)
+public void accountWithEmailAlreadyExists() throws Exception {
+    RegisterRequest registerRequest = new RegisterRequest();
+    registerRequest.setUsername("authTestUsername");
+    registerRequest.setEmail("authtest@mail.com");
+    registerRequest.setPassword("Testpass1234");
 
-        String jsonResponse = result.getResponse().getContentAsString();
-        JsonObject parsedResponse = JsonParser.parseString(jsonResponse).getAsJsonObject();
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/register")
+            .content(gson.toJson(registerRequest))
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isConflict())
+            .andExpect(jsonPath("$.status").value(409))
+            .andExpect(jsonPath("$.message").value("Email already exists"));
+}
 
-        assert(parsedResponse.get("status").getAsInt() == 409);
-        assert(parsedResponse.get("message").getAsString().contains("Email already exists"));
-    }
-    
-    //Test login
-    @Test
-    @Order(4)
-    public void testLogin() throws Exception {
-        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
-        authenticationRequest.setEmail("authtest@mail.com");
-        authenticationRequest.setPassword("Testpass1234");
-   
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonParserUtil.asJsonString(authenticationRequest)))
-                .andExpect(status().isOk())
-                .andReturn();
-   
-        String jsonResponse = result.getResponse().getContentAsString();
-        JsonObject parsedResponse = JsonParser.parseString(jsonResponse).getAsJsonObject();
-        
-        assert(parsedResponse.get("status").getAsInt() == 200);
-        assert(parsedResponse.get("message").getAsString().contains("User authenticated successfully"));
-        assert(parsedResponse.get("results").getAsJsonObject().get("access_token").getAsString() != null);
-        assert(parsedResponse.get("results").getAsJsonObject().get("refresh_token").getAsString() != null);
-    
-        accessToken = parsedResponse.get("results").getAsJsonObject().get("access_token").getAsString();
-        refreshToken = parsedResponse.get("results").getAsJsonObject().get("refresh_token").getAsString();
-    }
+// Test login
+@Test
+@Order(4)
+public void testLogin() throws Exception {
+    AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+    authenticationRequest.setEmail("authtest@mail.com");
+    authenticationRequest.setPassword("Testpass1234");
 
-    //Test invalid credentials login
-    @Test
-    @Order(5)
-    public void testInvalidCredentialsLogin() throws Exception {
+    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(gson.toJson(authenticationRequest)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value(200))
+            .andExpect(jsonPath("$.message").value("User authenticated successfully"))
+            .andExpect(jsonPath("$.results.access_token").isString())
+            .andExpect(jsonPath("$.results.refresh_token").isString())
+            .andReturn();
 
-        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
-        authenticationRequest.setEmail("authtest@mail.com");
-        authenticationRequest.setPassword("Testpass1");
-        
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonParserUtil.asJsonString(authenticationRequest)))
-                .andExpect(status().isUnauthorized())
-                .andReturn();
+    String jsonResponse = result.getResponse().getContentAsString();
+    JsonObject parsedResponse = JsonParser.parseString(jsonResponse).getAsJsonObject();
 
-        assert(result.getResponse().getContentAsString().contains("Invalid email or password"));
-        
-        String jsonResponse = result.getResponse().getContentAsString();
-        JsonObject parsedResponse = JsonParser.parseString(jsonResponse).getAsJsonObject();
+    accessToken = parsedResponse.get("results").getAsJsonObject().get("access_token").getAsString();
+    refreshToken = parsedResponse.get("results").getAsJsonObject().get("refresh_token").getAsString();
+}
 
-        assert(parsedResponse.get("status").getAsInt() == 401);
-        assert(parsedResponse.get("message").getAsString().contains("Invalid email or password"));
+// Test invalid credentials login
+@Test
+@Order(5)
+public void testInvalidCredentialsLogin() throws Exception {
+    AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+    authenticationRequest.setEmail("authtest@mail.com");
+    authenticationRequest.setPassword("Testpass1");
 
-    }
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(gson.toJson(authenticationRequest)))
+            .andExpect(status().isUnauthorized())
+            .andExpect(jsonPath("$.status").value(401))
+            .andExpect(jsonPath("$.message").value("Invalid email or password"));
+}
 
-    //Test refresh token
-    @Test
-    @Order(6)
-    public void testRefreshToken() throws Exception {
-
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/refresh-token")
-                .header("Authorization", "Bearer " + refreshToken)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
-        
-        String jsonResponse = result.getResponse().getContentAsString();
-        JsonObject parsedResponse = JsonParser.parseString(jsonResponse).getAsJsonObject();
-
-        assert(parsedResponse.get("status").getAsInt() == 200);
-        assert(parsedResponse.get("message").getAsString().contains("Token refreshed successfully"));
-        assert(parsedResponse.get("results").getAsJsonObject().get("access_token").getAsString() != null);
-        assert(parsedResponse.get("results").getAsJsonObject().get("refresh_token").getAsString() != null);
-
-    }
+// Test refresh token
+@Test
+@Order(6)
+public void testRefreshToken() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/refresh-token")
+            .header("Authorization", "Bearer " + refreshToken)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value(200))
+            .andExpect(jsonPath("$.message").value("Token refreshed successfully"))
+            .andExpect(jsonPath("$.results.access_token").isString())
+            .andExpect(jsonPath("$.results.refresh_token").isString());
+}
 
 }
