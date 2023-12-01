a731887192e588acb9cce3c63e30b4963a461967
jakottelaar <jarno.kottelaar@gmail.com>
2023-12-01 13:01:07 +0100

    Added channel type to channels

--- src/main/java/com/example/shaden/features/channel/Channel.java
+++ src/main/java/com/example/shaden/features/channel/Channel.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,5 +29,9 @@ public abstract class Channel {
 
     @Column(name = "last_modified_date")
     private LocalDateTime lastModifiedDate;
+
+    @Column(name = "channel_type")
+    @Enumerated(EnumType.STRING)
+    private ChannelType channelType;
 
 }
